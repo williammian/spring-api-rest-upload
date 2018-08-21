f3c02d4f18f35d1d6bf4bb8cc47eee1e16afbba8
willi <willi@LAPTOP-B6ER7NR3>
2018-08-21 06:59:19 -0300

    spring-api-rest-upload

---
+++ src/main/java/br/com/wm/contato/ContatoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wm.contato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatoApiApplication.class, args);
+	}
+	
+}
---
+++ src/main/java/br/com/wm/contato/resource/FotosResource.java
@@ -0,0 +1,24 @@
+package br.com.wm.contato.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.wm.contato.storage.Disco;
+
+@RestController
+@RequestMapping("/fotos")
+public class FotosResource {
+	
+	@Autowired
+	private Disco disco;
+	
+	@PostMapping
+	public void upload(@RequestParam MultipartFile foto) {
+		disco.salvarFoto(foto);
+	}
+
+}
